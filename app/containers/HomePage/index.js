6f32cf1503cd725ecb1ffbe0bbf0db65f9374816
alexanderturinske <a.turinske@gmail.com>
2019-12-16 07:26:37 -0700

    Add styling to homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -4,7 +4,7 @@
  * This is the first thing users see of our App, at the '/' route
  *
  */
-
+import './homepage.scss';
 import React from 'react';
 import SearchArea from '../SearchArea';
 import SearchResults from '../SearchResults';
@@ -12,10 +12,14 @@ import Favorites from '../Favorites';
 
 export default function HomePage() {
   return (
-    <div>
-      <SearchArea />
-      <SearchResults />
-      <Favorites />
+    <div className="homepage-container">
+      <div className="left-column">
+        <SearchArea />
+        <SearchResults />
+      </div>
+      <div className="right-column">
+        <Favorites />
+      </div>
     </div>
   );
 }
