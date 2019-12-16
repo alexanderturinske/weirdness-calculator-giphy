a78ba819d43498c39e2f57d891ef1348da7e4165
alexanderturinske <a.turinske@gmail.com>
2019-12-15 20:15:36 -0700

    Add styling for header

--- app/containers/Header/index.js
+++ app/containers/Header/index.js
@@ -5,13 +5,14 @@
  *
  */
 
+import './header.scss';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
 export default function Header() {
   return (
-    <h1>
+    <h1 className="header">
       <FormattedMessage {...messages.header} />
     </h1>
   );
