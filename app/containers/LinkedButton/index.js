027ef2c03c2b1be8d097ad117085d189f385c4d9
alexanderturinske <a.turinske@gmail.com>
2019-12-15 17:55:13 -0700

    Fix code styling
    - remove unnecessary curly braces

--- app/containers/LinkedButton/index.js
+++ app/containers/LinkedButton/index.js
@@ -9,23 +9,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router'
 
-const LinkButton = ({ history, onClick = _.noop, to, staticContext, ...rest }) => {
+const LinkButton = ({
+  history,
+  onClick = _.noop,
+  to,
+  staticContext,
+  ...rest
+}) => (
   // ^ filtering out props that `button` doesn’t know what to do with.
-  return (
-    <button
-      type="submit"
-      {...rest}
-      onClick={event => {
-        onClick && onClick(event);
-        history.push(to);
-      }}
-    />
-  );
-};
+  <button
+    type="submit"
+    {...rest}
+    onClick={event => {
+      // eslint-disable-next-line no-unused-expressions
+      onClick && onClick(event);
+      history.push(to);
+    }}
+  />
+);
 
 LinkButton.propTypes = {
   history: PropTypes.object.isRequired,
   onClick: PropTypes.func,
+  staticContext: PropTypes.object,
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
